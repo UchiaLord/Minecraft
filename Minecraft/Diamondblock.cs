1aae9bb7508faa43a0b80a4502f1da00ac7311b8
Pluto <benny@lol.at>
2026-03-05 14:54:24 +0100

    fixed

--- Minecraft/Diamondblock.cs
+++ Minecraft/Diamondblock.cs
@@ -8,14 +8,14 @@ namespace Minecraft
 {
     internal class Diamondblock : Block
     {
-        public Diamondblock() : base("Obsidian", 40, "GoldSpitzHacke", BlockColor.Turquoise, false) { }
+        public Diamondblock() : base("Diamant", 40, "GoldSpitzHacke", BlockColor.Turquoise, false) { }
 
         public override bool Dismantle(Tools tool) 
         {
 
             base.SetConsoleColor();
 
-            if (tool.Type != ToolType.GoldSpitzHacke)
+            if (tool.Type != ToolType.GoldSpitzHacke && tool.Type != ToolType.DiamandSpitzHacke)
             {
                 Console.WriteLine("-> Das Tool ist zu schwach für Diamant! (0 Schaden)");
                 return false; 
@@ -23,11 +23,11 @@ namespace Minecraft
 
             
             CurrentHp -= tool.Power;
-            Console.WriteLine($"Diamant-Power! Obsidian HP: {Math.Max(0, CurrentHp)}/{maxHealth}");
+            Console.WriteLine($"Diamant-Power! Diamant HP: {Math.Max(0, CurrentHp)}/{maxHealth}");
 
             if (CurrentHp <= 0)
             {
-                Console.WriteLine(">>> Obsidian wurde erfolgreich abgebaut! <<<");
+                Console.WriteLine(">>> Diamant wurde erfolgreich abgebaut! <<<");
             }
 
             return true; 
